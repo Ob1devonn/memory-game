2e013fbbab212d235cb264b68622d186cc9ca7e5
DeVonn <ob1devonn@gmail.com>
2023-11-28 17:34:11 -0500

    Build Color mode switch

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,12 @@
-import { HStack, Image, Text } from "@chakra-ui/react";
+import { HStack, Image } from "@chakra-ui/react";
 import logo from "../assets/harry_potter_icon_by_rickyrgoetz_dbfr8a2.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
 
 export default function Header() {
   return (
-    <HStack padding="2rem">
+    <HStack padding="2rem" justifyContent="space-between">
       <Image src={logo} boxSize="60px" />
-      <Text> Header</Text>
+      <ColorModeSwitch />
     </HStack>
   );
 }
