a571e08350621422e8f4e24b326cf57c89ca91a7
DeVonn <ob1devonn@gmail.com>
2023-11-28 16:52:40 -0500

    Create responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,23 @@
-import { Button } from "@chakra-ui/react";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 function App() {
   return (
     <div>
-      <Button>Hello World</Button>
+      <Grid
+        templateAreas={`"header header"
+                        "game game"
+                        "settings settings"`}
+      >
+        <GridItem area="header" bg="coral">
+          Header
+        </GridItem>
+        <GridItem area="game" bg="gold">
+          game
+        </GridItem>
+        <GridItem area="settings" bg="dodgerblue">
+          settings
+        </GridItem>
+      </Grid>
     </div>
   );
 }
